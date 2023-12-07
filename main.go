7ca60a399b696f80c52d6464f2b9f94dff7eb087
Faniloniaina RAMILISON <faniloniainaa21@gmail.com>
2023-12-07 23:33:11 +0300

    chore : refactor code

--- main.go
+++ main.go
@@ -3,101 +3,37 @@ package main
 import (
 	"fmt"
 	"os"
-	"os/exec"
 	"path/filepath"
-	"strings"
 	"sync"
 
-	"github.com/fsnotify/fsnotify"
+	"github.com/Faanilo/goload/utils"
 )
 
 func main() {
-	// Get the arguments passed to your program
-	args := os.Args[1:]
-	if len(args) < 1 {
+	fileToRun := utils.getFilePathFromArgs(os.Args)
+	if fileToRun == "" {
 		fmt.Println("Please provide the path to the Go file to run.")
 		return
 	}
-	fileToRun := args[0]
 
-	// get path of the file to run
-	absPath, err := filepath.Abs(fileToRun)
+	watcher, err := utils.setupWatcher(fileToRun)
 	if err != nil {
-		fmt.Println("Error getting absolute path:", err)
-		return
-	}
-
-	// waiting for the change in code
-	watcher, err := fsnotify.NewWatcher()
-	if err != nil {
-		fmt.Println("Error creating watcher:", err)
+		fmt.Println("Error setting up watcher:", err)
 		return
 	}
 	defer watcher.Close()
 
-	// Add the directory containing the file to the watcher
-	dir := filepath.Dir(absPath)
-	err = watcher.Add(dir)
-	if err != nil {
-		fmt.Println("Error adding directory to watcher:", err)
-		return
-	}
+	fmt.Println("Watching for changes in", filepath.Dir(fileToRun))
 
-	fmt.Println("Watching for changes in", dir)
-
-	// Set up a goroutine to handle file change events
-	var wg sync.WaitGroup
+	wg := &sync.WaitGroup{}
 	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		for {
-			select {
-			case event, ok := <-watcher.Events:
-				if !ok {
-					return
-				}
-				if event.Op&fsnotify.Write == fsnotify.Write {
-					fmt.Println("Detected change in", event.Name)
-					if strings.HasSuffix(event.Name, ".go") {
-						fmt.Println("Restarting the application...")
-						if err := restartApp(fileToRun); err != nil {
-							fmt.Println("Error restarting application:", err)
-						}
-					}
-				}
-			case err, ok := <-watcher.Errors:
-				if !ok {
-					return
-				}
-				fmt.Println("Error watching:", err)
-			}
-		}
-	}()
+	go utils.WatchChanges(fileToRun, watcher, wg)
 
-	// Run the initial instance of the application
 	fmt.Println("Running", fileToRun)
-	if err := runServer(fileToRun); err != nil {
+	if err := utils.StartServer(fileToRun); err != nil {
 		fmt.Println("Error running server:", err)
-		wg.Wait() // Wait for file changes despite the initial error
+		wg.Wait()
 	} else {
 		wg.Wait()
 	}
-}
-
-func restartApp(file string) error {
-	// Kill the current process
-	os.Exit(0)
-	// Uncomment the lines below if you want to spawn a new process instead of exiting the current one
-	// runCmd := exec.Command("go", "run", file)
-	// runCmd.Stdout = os.Stdout
-	// runCmd.Stderr = os.Stderr
-	// return runCmd.Start()
-	return nil
-}
-
-func runServer(file string) error {
-	runCmd := exec.Command("go", "run", file)
-	runCmd.Stdout = os.Stdout
-	runCmd.Stderr = os.Stderr
-	return runCmd.Run()
 }
---
+++ utils/utils.go
@@ -0,0 +1,79 @@
+package utils
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"sync"
+
+	"github.com/fsnotify/fsnotify"
+)
+
+func getFilePathFromArgs(args []string) string {
+	if len(args) < 2 {
+		return ""
+	}
+	return args[1]
+}
+
+func setupWatcher(fileToRun string) (*fsnotify.Watcher, error) {
+	absPath, err := filepath.Abs(fileToRun)
+	if err != nil {
+		return nil, err
+	}
+
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		return nil, err
+	}
+
+	dir := filepath.Dir(absPath)
+	if err := watcher.Add(dir); err != nil {
+		return nil, err
+	}
+
+	return watcher, nil
+}
+
+func watchChanges(fileToRun string, watcher *fsnotify.Watcher, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for {
+		select {
+		case event, ok := <-watcher.Events:
+			if !ok {
+				return
+			}
+			if event.Op&fsnotify.Write == fsnotify.Write && strings.HasSuffix(event.Name, ".go") {
+				fmt.Println("Detected change in", event.Name)
+				fmt.Println("Restarting the application...")
+				if err := restartApp(fileToRun); err != nil {
+					fmt.Println("Error restarting application:", err)
+				}
+			}
+		case err, ok := <-watcher.Errors:
+			if !ok {
+				return
+			}
+			fmt.Println("Error watching:", err)
+		}
+	}
+}
+
+func restartApp(file string) error {
+	os.Exit(0)
+	// Uncomment the lines below if you want to spawn a new process instead of exiting the current one
+	// runCmd := exec.Command("go", "run", file)
+	// runCmd.Stdout = os.Stdout
+	// runCmd.Stderr = os.Stderr
+	// return runCmd.Start()
+	return nil
+}
+
+func startServer(file string) error {
+	runCmd := exec.Command("go", "run", file)
+	runCmd.Stdout = os.Stdout
+	runCmd.Stderr = os.Stderr
+	return runCmd.Run()
+}
